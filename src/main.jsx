69e85d35b58d9ce022e63630901af08a0f606710
Behruz <madaminovbehruz746@gmail.com>
2023-12-28 15:58:32 +0500

    test: 22

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,12 @@ import "./index.css";
 import "normalize.css";
 import { registerLicense } from "@syncfusion/ej2-base";
 import { ContextProvider } from "./contexts/ContextProvider";
+import { Suspense, lazy } from "react";
+const Loading = lazy(() => (
+  <div>
+    <h1 className="text-[#000]">Loading...</h1>
+  </div>
+));
 
 registerLicense(
   "Ngo9BigBOggjHTQxAR8/V1NAaF5cWWJCf0x0R3xbf1x0ZFZMY1pbRnFPMyBoS35RdURhW3xedHVUR2hcUUd0"
@@ -14,9 +20,11 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
-  </StrictMode>
+  <Suspense fallback={<Loading />}>
+    <StrictMode>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </StrictMode>
+  </Suspense>
 );
