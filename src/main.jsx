11ccb0c845cbcdec6f86a4fc6280c289948c4d87
Behruz <madaminovbehruz746@gmail.com>
2023-12-28 16:06:19 +0500

    fixed: bug in files

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,6 @@ import "./index.css";
 import "normalize.css";
 import { registerLicense } from "@syncfusion/ej2-base";
 import { ContextProvider } from "./contexts/contextProvider";
-import { Suspense, lazy } from "react";
-const Loading = lazy(() => (
-  <div>
-    <h1 className="text-[#000]">Loading...</h1>
-  </div>
-));
 
 registerLicense(
   "Ngo9BigBOggjHTQxAR8/V1NAaF5cWWJCf0x0R3xbf1x0ZFZMY1pbRnFPMyBoS35RdURhW3xedHVUR2hcUUd0"
@@ -20,11 +14,9 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <Suspense fallback={<Loading />}>
-    <StrictMode>
-      <ContextProvider>
-        <App />
-      </ContextProvider>
-    </StrictMode>
-  </Suspense>
+  <StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </StrictMode>
 );
